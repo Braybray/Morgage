0037fde32ba351df1c2c1bc1532a463120d7e758
unknown <jiangzha@PBB-C231-14.iowa.uiowa.edu>
2013-02-19 12:09:49 -0600

    made changes.

--- Default.aspx
+++ Default.aspx
@@ -10,6 +10,31 @@
     <form id="form1" runat="server">
     <div>
     
+        Principle Amount:<br />
+        <asp:TextBox ID="LoanAmount" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        Annual Interest Rate:<br />
+        <asp:TextBox ID="Rate" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <br />
+        Morgage Length:
+        <br />
+        <asp:TextBox ID="MorgageLength" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <asp:Button ID="PerformCalcButton" runat="server" Text="Compute Monthly Cost" 
+            Width="150px" />
+        <br />
+        <br />
+        <asp:Label ID="Results" runat="server"></asp:Label>
+        <br />
+        <br />
+    
     </div>
     </form>
 </body>
--- Default.aspx.vb
+++ Default.aspx.vb
@@ -2,4 +2,49 @@
 Partial Class _Default
     Inherits System.Web.UI.Page
 
+    Protected Sub TextBox3_TextChanged(sender As Object, e As System.EventArgs) Handles MorgageLength.TextChanged
+
+    End Sub
+
+    Protected Sub PerformCalcButton_Click(sender As Object, e As System.EventArgs) Handles PerformCalcButton.Click
+
+        'Specify constant values
+        Const INTEREST_CALCS_PER_YEAR As Integer = 12
+        Const PAYMENTS_PER_YEAR As Integer = 12
+
+        'Create variable to hold the values entered by the users
+        Dim P As Decimal = LoanAmount.Text
+        Dim r As Decimal = Rate.Text / 100
+        Dim t As Decimal = MorgageLength.Text
+
+        Dim ratePerperiod As Decimal
+        ratePerperiod = r / INTEREST_CALCS_PER_YEAR
+
+        Dim PayPeriods As Integer
+        PayPeriods = t * PAYMENTS_PER_YEAR
+
+        Dim annualRate As Decimal
+        annualRate = Math.Exp(INTEREST_CALCS_PER_YEAR * Math.Log(1 + ratePerperiod)) - 1
+
+        Dim intPerPayment As Decimal
+        intPerPayment = (Math.Exp(Math.Log(annualRate + 1) / PayPeriods) - 1) * PayPeriods
+        'nOW COMPUTE THE TOTAL COST OF THE LOAN
+
+        Dim intPerMonth As Decimal = intPerPayment / PAYMENTS_PER_YEAR
+
+        Dim costPerMonth As Decimal
+        costPerMonth = P * intPerMonth / (1 - Math.Pow(intPerMonth + 1, -PayPeriods))
+
+        'Display the results in the results label web Control
+        Results.Text = "Your morgage payment per month is $" & costPerMonth.ToString("#,####.##")
+
+
+
+
+
+
+
+
+
+    End Sub
 End Class
